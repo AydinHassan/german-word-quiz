90d53c234db3d8c5f5922d486c22cae9caa38a24
Aydin Hassan <aydin@hotmail.co.uk>
2026-01-27 19:33:12 +0100

    feat: improve

--- functions/words.js
+++ functions/words.js
@@ -8,3 +8,17 @@ export async function onRequestPost({ request, env }) {
   return Response.json({ ok: true })
 }
 
+export async function onRequestDelete({ request, env }) {
+  const { id } = await request.json()
+  await env.DB.prepare("DELETE FROM words WHERE id = ?").bind(id).run()
+  return new Response(JSON.stringify({ ok: true }), {
+    headers: { "Content-Type": "application/json" }
+  })
+}
+
+export async function onRequestGet({ env }) {
+  const words = await env.DB.prepare("SELECT * FROM words ORDER BY created_at DESC").all()
+  return new Response(JSON.stringify(words.results), {
+    headers: { "Content-Type": "application/json" }
+  })
+}
